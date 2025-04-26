b035d3691905c2444626309d356e2514dffbdd15
fpaiz-2022134 <fralpaiz@gmail.com>
2025-04-26 13:54:24 -0600

    Creating branch for edgar

---
+++ data/program_data.py
@@ -0,0 +1,8 @@
+usuarios = {}
+facturas_pendientes = []
+
+#funcion para validar que lo ingresado sea texto y no alfanúmerico
+#funcion para validar que lo ingresado sea númerico
+
+#si el valor es texto, devolver texto
+#si es númerico, devolver númerico
